1987bef720ec27fc74dab176e1717fc4bfe3da72
aytang <albertyptang@gmail.com>
2015-06-29 20:02:06 -0700

    deploy

--- config/googleConfig.js
+++ config/googleConfig.js
@@ -1,9 +1,9 @@
 
 //google clientId and clientSecret made from https://console.developers.google.com/project
 var googleConfig = {
-    clientID: '189160676278-ci6rhq98n64ig2ekbb4ojst990u6tg6s.apps.googleusercontent.com',
-    clientSecret: 'SkWnFwdTOG69OgDZi092hHqC',
-    callbackURL: "http://127.0.0.1:3000/auth/google/callback"
+    clientID: '706416071848-o2mii1b1r6th1hn5k4s4j6upkm5gm9l9.apps.googleusercontent.com',
+    clientSecret: 'ooHjHT0yLXXrOPBwW97LdCKN',
+    callbackURL: "https://munibutler.herokuapp.com/auth/google/callback"
   };
 
 module.exports = googleConfig;
